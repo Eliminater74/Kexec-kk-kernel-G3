3e2c647b9cbddf93d9731d564aa52608c1e56543
Skin1980 <skin1980@gmail.com>
2015-01-11 17:23:30 +0100

    Initial Release

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Jan 11 16:44:47 CET 2015"
+#define LINUX_COMPILE_BY "skin1980"
+#define LINUX_COMPILE_HOST "skin1980pc"
+#define LINUX_COMPILER "gcc version 4.8.4 20140719 (prerelease) (SaberMod-4.8.4_arm-graphite) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-perf-g13ed2b4-dirty"
